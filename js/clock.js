020f29daad0c1e9942423c9dcb30be378359fd25
hong2goon <hong2@hong2ui-MacBookAir.local>
2020-01-06 23:09:00 +0900

    first commit

---
+++ js/clock.js
@@ -0,0 +1,27 @@
+const clockContainer = document.querySelector('.js-clock'),
+  clockTime = clockContainer.querySelector('.time'),
+  clockFormat = clockContainer.querySelector('.format');
+
+function getTime() {
+  const date = new Date(),
+    hours = date.getHours(),
+    hours12 = hours % 12,
+    minutes = date.getMinutes(),
+    seconds = date.getSeconds(),
+    amOrPm = date.toLocaleTimeString('en-US').split(' ')[1];
+  
+  if (clockContainer.classList.contains('hour12')) {
+    clockFormat.classList.add('show');
+    clockFormat.innerText = amOrPm;
+    clockTime.innerHTML = `<span class="hours">${hours12 < 10 ? `0${hours12}` : hours12}</span>:<span class="minutes">${minutes < 10 ? `0${minutes}` : minutes}</span><span class="seconds">:${seconds < 10 ? `0${seconds}` : seconds}</span>`;
+  } else {
+    clockFormat.classList.remove('show');
+    clockTime.innerHTML = `<span class="hours">${hours < 10 ? `0${hours}` : hours}</span>:<span class="minutes">${minutes < 10 ? `0${minutes}` : minutes}</span><span class="seconds">:${seconds < 10 ? `0${seconds}` : seconds}</span>`;
+  }
+}
+
+function clockInit() {
+  getTime();
+  setInterval(getTime, 1000);
+}
+clockInit();
