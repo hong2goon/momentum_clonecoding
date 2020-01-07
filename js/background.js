42cbfd9b497dc5919f470f75b18a54404d1e3aca
hong2goon <hong2goon@gmail.com>
2020-01-07 15:09:52 +0900

    background 추가

---
+++ js/background.js
@@ -0,0 +1,29 @@
+const body = document.querySelector("body"),
+    cover = body.querySelector(".cover");
+
+const IMG_NUMBER = 3;
+
+function handleImgLoad() {
+    console.log("finished loading");
+}
+
+function paintImage(imgNumber) {
+    const image = new Image(), 
+        imageWrap = document.createElement('div');
+    image.src = `../images/${imgNumber + 1}.jpg`;
+    imageWrap.classList.add("bgImage");
+    imageWrap.appendChild(image);
+    cover.appendChild(imageWrap);
+    image.addEventListener("loadend", handleImgLoad);
+}
+
+function genRandom() {
+    const number = Math.floor(Math.random() * IMG_NUMBER);
+    return number;
+}
+
+function bgInit() {
+    const randomNumber = genRandom();
+    paintImage(randomNumber);
+}
+bgInit();
